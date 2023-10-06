e7373690deef1c2b7b19f3d782ef88cfe7e4f82f
admin <admin@gmail.com>
2023-10-06 11:38:58 -0700

    Menambahkan file script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+// Contoh penggunaan if-else
+// let usia = 17;
+
+// if (usia < 20) {
+//   console.log("Terus belajar ya karena dunia kerja itu kejam 😮‍💨");
+// } else {
+//   console.log("Kerja lah woi jangan rebahan mulu 👊");
+// }
+
+// Contoh penggunaan nested if
+// let adzan = "shubuh";
+
+// if (adzan === "shubuh") {
+//   console.log("Saatnya sholat shubuh jangan ngorok mulu");
+// } else if (adzan === "dzuhur") {
+//   console.log("Saatnya sholat dzuhur jangan kerja mulu");
+// } else if (adzan === "ashar") {
+//     console.log("Saatnya sholat ashar jangan tidur siang dong");
+// } else if (adzan === "magrib") {
+//     console.log("Saatnya sholat magrib jangan nyore mulu");
+// } else if (adzan === "isya") {
+//     console.log("Saatnya sholat isya jangan nongkrong mulu")
+// }
+//  else {
+//   console.log("Sholat lah woi wajib tuh");
+// }
+
+// Contoh penggunaan switch case
+// let waktu = "pag";
+
+// switch (waktu) {
+//   case "pagi":
+//     console.log("Saatnya sarapan");
+//     break;
+//   case "siang":
+//     console.log("Saatnya makan siang");
+//     break;
+//     case "malam":
+//         console.log("Saatnya makan malam")
+//   default:
+//     console.log("Makan terus kerja kaga hehehe");
+// }
+
+// Contoh penggunaan for statement
+// for (let i = 1; i <= 10; i++) {
+//   console.log("Fast Furious Episode : " + i);
+// }
+
+// Contoh penggunaan while
+// let pinjam = 100;
+
+// while (pinjam <= 500) {
+//   console.log("Pinjem " + pinjam + " nanti gua balikin");
+//   pinjam+=100;
+// }
+
+// Contoh penggunaan do-while
+// let pinjam = 100;
+
+// do {
+//   console.log("Pinjam " + pinjam + " nanti gua balikin");
+//   pinjam+= 100;
+// } while (pinjam <= 500);
+
+// Contoh penggunaan function
+// function panggil(nama) {
+//   console.log("Hallo " + nama + " gantenggg 🥶");
+// }
+
+// panggil("Andre");
